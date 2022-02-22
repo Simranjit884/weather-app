b73d5f6fd6e6542722e77342d6c64c87202f166d
samy884 <ssgill884@gmail.com>
2022-02-22 18:51:08 +0530

    adding app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+const url = 'http://api.weatherstack.com/current?access_key=cc2439c7641b8e63086d36854c9f767e&query=37.8267,-122.4233';
+
+request({url: url}, (error, response) => {
+const data = JSON.parse(response.body);
+console.log(data);
+});
