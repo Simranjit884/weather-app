93082fa69313fa45767f3944927d640586fcaa94
samy884 <ssgill884@gmail.com>
2022-02-22 19:11:41 +0530

    changes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const request = require('request');
 
 const url = 'http://api.weatherstack.com/current?access_key=cc2439c7641b8e63086d36854c9f767e&query=37.8267,-122.4233';
 
-request({url: url}, (error, response) => {
-const data = JSON.parse(response.body);
-console.log(data);
+request({url: url, json: true}, (error, response) => {
+// const data = JSON.parse(response.body);
+// console.log(response.body.current);
+console.log(`it is currently ${response.body.current.temperature} but it feels like ${response.body.current.feelslike}`)
 });
